536d8f07924a56854367d45452300906d1916247
joey5515 <joeyrreno@gmail.com>
2021-05-01 12:04:22 -0400

    updated pedometer

--- app/src/main/java/com/example/fitnessappproject475/ui/Pedometer/PedometerViewModel.java
+++ app/src/main/java/com/example/fitnessappproject475/ui/Pedometer/PedometerViewModel.java
@@ -1,18 +1,35 @@
 package com.example.fitnessappproject475.ui.Pedometer;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class PedometerViewModel extends ViewModel {
-    private MutableLiveData<String> mText;
+    boolean activityRunning;
+    private MutableLiveData<String> mText = new MutableLiveData<>();
+    public String steps;
+
+    public interface OnSensorChangedListener {
+        void onSensorChange(SensorEvent event);
+    }
+
+    public void updateMtext(String steps) {
+        mText.setValue(steps);
+    }
 
     public PedometerViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is the pedometer fragment");
+        mText.setValue(steps);
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
+
 }
