4ca7b6a733040c5b0a87f79ea532b747960fe75e
spf100 <72419892+spf100@users.noreply.github.com>
2021-05-04 18:33:21 -0400

    touch the nav again i dare you

---
+++ app/src/main/java/com/example/fitnessappproject475/EntryDAO.java
@@ -0,0 +1,23 @@
+package com.example.fitnessappproject475;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.OnConflictStrategy;
+
+import java.util.List;
+
+
+@Dao
+public interface EntryDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Entry entry);
+
+    @Query("DELETE FROM entries")
+    void deleteAll();
+
+    @Query("SELECT * FROM entries")
+    LiveData<List<Entry>> getEntries();
+}
+
