424d83824e0e35724f08e57ad3c04f76ab8e98b4
spf100 <72419892+spf100@users.noreply.github.com>
2021-05-04 18:37:04 -0400

    touch the nav again i dare you

--- app/src/main/java/com/example/fitnessappproject475/Entry.java
+++ app/src/main/java/com/example/fitnessappproject475/Entry.java
@@ -1,37 +1,14 @@
 package com.example.fitnessappproject475;
 
-import androidx.annotation.NonNull;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "entries")
 public class Entry {
-    public static class Food {
-        private String name;
-        private int kilocal;
-        private double grams;
 
-        public String getName() { return name; }
-        public void setName(String name){ this.name = name;}
-        public int getKilocal(){return kilocal;}
-        public void setKilocal(int kilocal){this.kilocal = kilocal;}
-        public double getGrams(){return grams;}
-        public void setGrams(double grams){this.grams = grams;}
-        public Food(String name, int kilocal, double grams){
-            setName(name);
-            setKilocal(kilocal);
-            setGrams(grams);
-        }
-        @NonNull
-        @Override
-        public String toString(){
-            StringBuilder builder = new StringBuilder();
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    public int getId() { return id; }
+    public void setId(int id) {id = this.id;  }
 
-            builder.append("( ")
-                    .append(getName()).append(' ')
-                    .append(getKilocal()).append(' ')
-                    .append(getGrams()).append(' ')
-                    .append(')');
-            return builder.toString();
-        }
-    }
 }
